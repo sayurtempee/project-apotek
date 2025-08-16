d4b3eacacdee1a72016ff8691326f6565a0870d7
kazuo <kazuo@kazuo-mii>
2025-08-16 21:34:17 +0700

    add new unit features

---
+++ database/migrations/2025_08_16_173342_add_unit_id_to_obats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('obats', function (Blueprint $table) {
+            $table->unsignedBigInteger('unit_id')->nullable()->after('stok');
+
+            $table->foreign('unit_id')->references('id')->on('units')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('obats', function (Blueprint $table) {
+            $table->dropForeign(['unit_id']);
+            $table->dropColumn('unit_id');
+        });
+    }
+};
